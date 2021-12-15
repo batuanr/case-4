53cf94293375cb78d7f34677e25594902fa97c50
unknown <dinh.kawa.98@gmail.com>
2021-12-15 17:26:16 +0700

    update tuan

--- src/main/java/com/example/case_module_4/repository/ICategoryRepository.java
+++ src/main/java/com/example/case_module_4/repository/ICategoryRepository.java
@@ -2,6 +2,8 @@ package com.example.case_module_4.repository;
 
 import com.example.case_module_4.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ICategoryRepository extends JpaRepository<Category,Long> {
 }
