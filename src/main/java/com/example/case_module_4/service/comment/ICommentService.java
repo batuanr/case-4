1b6810ee4160054f0610c09c2597e9f619edd02b
unknown <dinh.kawa.98@gmail.com>
2021-12-16 09:54:11 +0700

    update comment service and repo

---
+++ src/main/java/com/example/case_module_4/service/comment/ICommentService.java
@@ -0,0 +1,7 @@
+package com.example.case_module_4.service.comment;
+
+import com.example.case_module_4.model.Comment;
+import com.example.case_module_4.service.IGeneralService;
+
+public interface ICommentService extends IGeneralService<Comment> {
+}
