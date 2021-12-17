4901254579fb9c8532e5e5757e009b61e9c7d558
unknown <dinh.kawa.98@gmail.com>
2021-12-17 17:35:44 +0700

    update truong

--- src/main/java/com/example/case_module_4/service/comment/ICommentService.java
+++ src/main/java/com/example/case_module_4/service/comment/ICommentService.java
@@ -3,9 +3,12 @@ package com.example.case_module_4.service.comment;
 import com.example.case_module_4.model.Book;
 import com.example.case_module_4.model.Comment;
 import com.example.case_module_4.service.IGeneralService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
 public interface ICommentService extends IGeneralService<Comment> {
     Iterable<Comment> findAllByBook(Long id);
+    Page<Comment> findAll(Pageable pageable);
 }
